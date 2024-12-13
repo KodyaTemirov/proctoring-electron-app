690ece2ff44037b93ec02fe5e43c4675dd809ea1
Kodya Temirov <kodyatemirov@gmail.com>
2024-12-13 15:51:03 +0500

    feat: new version

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -4,17 +4,17 @@
   <div class="creator">Powered by Realsoft</div>
   <div class="text">
     RealSoft
-    <span class="vue">Proctoring App</span>
+    <span class="vue">Proctoring</span>
   </div>
   <p class="tip">Iltimos, oynani pastga olib qo'ying.</p>
   <div class="actions">
     <div class="action">
       <a href="https://realsoft.uz/" target="_blank" rel="noreferrer"
-        >Realsoft.uz</a
+        >RealSoft</a
       >
     </div>
     <div class="action">
-      <a target="_blank" href="http://proctoring.uz/">Proctoring.uz</a>
+      <a target="_blank" href="http://proctoring.uz/">Proctoring</a>
     </div>
   </div>
 </template>
